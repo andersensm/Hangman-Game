d08a00a6242e3383ea160a918faad1aa8151d235
Sean Andersen <smandersen13@gmail.com>
2017-11-25 18:26:40 -0500

    new initial .js

---
+++ assets/javascript/game.js
@@ -0,0 +1,54 @@
+//Array of different staple foods of USA //
+const wordBank = ['Key Lime Pie'
+'Tater Tots'
+'Sourdough Bread'
+'Cobb Salad'
+'Pot Roast'
+'Twinkies'
+'Jerky'
+'Fajitas'
+'Banana Split'
+'Cornbread'
+'Jambalaya'
+'Biscuits n Gravy'
+'Honey Ham'
+'California Roll'
+'Meatloaf'
+'Grits'
+'Macaroni n Cheese'
+'Maryland Crabcakes'
+'Potato Chips'
+'Fortune Cookies'
+'Peanut Butter Sandwich'
+'Baked Beans'
+'Popcorn'
+'Fried Chicken n Waffles'
+'New England Clam Chowder'
+'Smores'
+'Lobster Rolls'
+'Buffalo Wings'
+'Barbecue Ribs'
+'BLT'
+'Apple Pie'
+'Po Boy'
+'Green Chile Stew'
+'Chocolate Chip Cookies'
+'Blueberry Cobbler'
+'Chicago Style Pizza'
+'Nachos'
+'Philly Cheese Steak'
+'Hot Dogs'
+'Cheese Burger'
+'French Fries'
+'Beef Tacos'
+'Reuben Sandwich']
+//Choose Word Randomly //
+var randNum = Math.floor(Math.random() * wordBank.length);
+console.log(randNum);
+//Create Underscore based on length of words//
+
+//Get users guess//
+
+//Check if guess is right//
+
+//if right, push to right array//
