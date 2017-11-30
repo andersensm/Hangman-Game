fcee7d647f676f160432d4aa1c4b0f0712d92cd6
Sean Andersen <smandersen13@gmail.com>
2017-11-29 23:21:12 -0500

    added comments to .js file

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -83,7 +83,7 @@ console.log(underScore);
 startGame();
 
 function startGame() {
-
+			//Default settings for variables
 			underScore = [];
 			userGuess = [];
 			rightGuess = [];
@@ -95,50 +95,44 @@ function startGame() {
 				underScore.push('_');
 				//Removes the commas within the array and replaces with white space
 			}
+				//Push default settings to HTML DOM
 				document.getElementById('underscore').innerHTML = underScore.join(' ');
 				document.getElementById('rightGuess').innerHTML = rightGuess;
 				document.getElementById('wrongGuess').innerHTML = wrongGuess;
 				document.getElementById('guessWrongMaximum').innerHTML = guessWrongMaximum;
-
+//Any letter will initiate below script
 document.onkeyup = function(event) {
 
 				document.getElementById('winsTracker').innerHTML = wins;
 				document.getElementById('lossesTracker').innerHTML = losses;
-
-
+				//userGuess will store keycode used
 				var userGuess = event.key;
-
+				//Looks in lowercaseChosennWord index for keystroke
 				if (lowerChosenWord.indexOf(userGuess) > -1) {
-
-
+				//Adds keystroke to rightGuess array
 				rightGuess.push(userGuess);
-
-
+				//Push to HTML DOM
 				document.getElementById('rightGuess').innerHTML = rightGuess;
-
-
+				//If not 'right' then 'wrong'
 				} else {
-
 				wrongGuess.push(userGuess);
-
+				//Push to HTML DOM
 				document.getElementById('wrongGuess').innerHTML = wrongGuess;
-
 				guessWrongMaximum--;
-
+				//Push to HTML DOM
 				document.getElementById('guessWrongMaximum').innerHTML = guessWrongMaximum;
 
+							//Game finishes with '0' guesses remaining and restarts
 							if (guessWrongMaximum == 0) {
 								losses++;
-								document.getElementById('winsTracker').innerHTML = wins;
+								//Push to HTML DOM
 								document.getElementById('lossesTracker').innerHTML = losses;
-
-								//Choose random words within wordBank array
+								//Choose new random words within wordBank array for restart
 								chosenWord = wordBank[Math.floor(Math.random() * wordBank.length)];
 								lowerChosenWord = chosenWord.toLowerCase();
 								split = lowerChosenWord.split('');
 								wordCount = split.length;
-
-
+								//Restart Game
 								startGame();
 							}
 
@@ -151,6 +145,8 @@ document.onkeyup = function(event) {
 //If correct, update the 'underScore' to a letter and remove the 'underScore'.
 
 //Update the 'rightGuess'/'wrongGuess' arrays. If the user types the same keys again it shouldn't count as incorrect or correct, neight should be addded twice. It will have to check back with both guess arrays.
+
+//Add in script to compare var 'wordCount.length' vs var 'chosenWord.length' for achieving completion.
 
 //If the user completes the word, change the graphic to the image associated with the 'wordBank' array and the name within the array.
 
