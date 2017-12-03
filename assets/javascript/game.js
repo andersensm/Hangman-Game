50b8474db2f31a7319c4bbc93f600bb9943b0b04
Sean Andersen <smandersen13@gmail.com>
2017-12-03 13:30:24 -0500

    last updates and comments-game works

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,8 +6,7 @@
 //Array of different staple foods of USA //
 var wordBank = ['KeyLimePie',
 	'Tatertots',
-	'SourdoughBread']
-	/*
+	'SourdoughBread',
 	'CobbSalad',
 	'PotRoast',
 	'Twinkies',
@@ -49,13 +48,6 @@ var wordBank = ['KeyLimePie',
 	'BeefTacos',
 	'ReubenSandwich'
 ]
-*/
-
-
-var keylimepie = 'KeyLimePie';
-var tatertots = 'Tatertots';
-var sourdough = 'SourdoughBread';
-
 
 var alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x',
 'y','z'];
@@ -106,7 +98,7 @@ function defaultVariables() {
 }
 
 function underscore() {
-	//Create Underscore for each letter based on length of randomized words
+	//Create underscore for each letter based on length of randomized word
 	for (var i = 0; i < lowerChosenWord.length; i++) {
 					underScore.push('_');
 				}
@@ -121,20 +113,198 @@ function defaultHtmlDom() {
 }
 
 function winGraphic() {
-	//Insert Image and Words for particular word
+	//Inset Words & styles for particular word win
 	var picturename = "<strong> Delicious " + chosenWord + "! </strong>"
 	document.getElementById('pictitle').innerHTML = picturename;
 	document.getElementById('pictitle').style.color = "white";
 	document.getElementById('pictitle').style.background = "green";
 
-	if (chosenWord === sourdough) {
-	document.getElementById("winimg").src="./assets/images/SourdoughBread.jpg"
-} else if (chosenWord === tatertots) {
-	document.getElementById("winimg").src="./assets/images/TaterTots.jpg"
-} else if (chosenWord === keylimepie) {
-	document.getElementById("winimg").src="./assets/images/KeyLimePie.jpg"
+	//Graphic inserted into HTML DOM if chosenWord = any of the strings below
+	if (chosenWord === 'ApplePie') {
+		document.getElementById("winimg").src="./assets/images/ApplePie.jpg"
+	}else if (chosenWord === 'BakedBeans') {
+		document.getElementById("winimg").src="./assets/images/BakedBeans.jpg"
+	}else if (chosenWord === 'BananaSplit') {
+		document.getElementById("winimg").src="./assets/images/BananaSplit.jpg"
+	}else if (chosenWord === 'BarbecueRibs') {
+		document.getElementById("winimg").src="./assets/images/BarbecueRibs.jpg"
+	}else if (chosenWord === 'BeefTacos') {
+		document.getElementById("winimg").src="./assets/images/BeefTacos.jpg"
+	}else if (chosenWord === 'BiscuitsNgravy') {
+		document.getElementById("winimg").src="./assets/images/Biscuits&gravy.jpg"
+	}else if (chosenWord === 'BLT') {
+		document.getElementById("winimg").src="./assets/images/Blt.jpg"
+	}else if (chosenWord === 'BlueBerryCobbler') {
+		document.getElementById("winimg").src="./assets/images/BlueberryCobbler.jpg"
+	}else if (chosenWord === 'BuffaloWings') {
+		document.getElementById("winimg").src="./assets/images/BuffaloWings.jpg"
+	}else if (chosenWord === 'CaliforniaRoll') {
+		document.getElementById("winimg").src="./assets/images/CaliforniaRoll.jpg"
+	}else if (chosenWord === 'CheeseBurger') {
+		document.getElementById("winimg").src="./assets/images/CheeseBurger.jpg"
+	}else if (chosenWord === 'ChicagoStylePizza') {
+		document.getElementById("winimg").src="./assets/images/ChicagoStylePizza.jpg"
+	}else if (chosenWord === 'ChocolateChipCookies') {
+		document.getElementById("winimg").src="./assets/images/ChocolateChipCookies.jpg"
+	}else if (chosenWord === 'CobbSalad') {
+		document.getElementById("winimg").src="./assets/images/CobbSalad.jpg"
+	}else if (chosenWord === 'Cornbread') {
+		document.getElementById("winimg").src="./assets/images/Cornbread.jpg"
+	}else if (chosenWord === 'Fajitas') {
+		document.getElementById("winimg").src="./assets/images/Fajitas.jpg"
+	}else if (chosenWord === 'FortuneCookies') {
+		document.getElementById("winimg").src="./assets/images/FortuneCookies.jpg"
+	}else if (chosenWord === 'FrenchFries') {
+		document.getElementById("winimg").src="./assets/images/FrenchFries.jpg"
+	}else if (chosenWord === 'FriedChickenNWaffles') {
+		document.getElementById("winimg").src="./assets/images/FriedChicken&waffles.jpg"
+	}else if (chosenWord === 'GreenChileStew') {
+		document.getElementById("winimg").src="./assets/images/GreenChileStew.jpg"
+	}else if (chosenWord === 'Grits') {
+		document.getElementById("winimg").src="./assets/images/Grits.jpg"
+	}else if (chosenWord === 'HoneyHam') {
+		document.getElementById("winimg").src="./assets/images/HoneyHam.jpg"
+	}else if (chosenWord === 'HotDogs') {
+		document.getElementById("winimg").src="./assets/images/HotDogs.jpg"
+	}else if (chosenWord === 'Jambalaya') {
+		document.getElementById("winimg").src="./assets/images/Jambalaya.jpg"
+	}else if (chosenWord === 'Jerky') {
+		document.getElementById("winimg").src="./assets/images/Jerky.jpg"
+	}else if (chosenWord === 'KeyLimePie') {
+		document.getElementById("winimg").src="./assets/images/KeyLimePie.jpg"
+	}else if (chosenWord === 'LobsterRolls') {
+		document.getElementById("winimg").src="./assets/images/LobsterRolls.jpg"
+	}else if (chosenWord === 'MacaroniNCheese') {
+		document.getElementById("winimg").src="./assets/images/Macaroni&cheese.jpg"
+	}else if (chosenWord === 'MarylandCrabcakes') {
+		document.getElementById("winimg").src="./assets/images/MarylandCrabcakes.jpg"
+	}else if (chosenWord === 'Meatloaf') {
+		document.getElementById("winimg").src="./assets/images/Meatloaf.jpg"
+	}else if (chosenWord === 'Nachos') {
+		document.getElementById("winimg").src="./assets/images/Nachos.jpg"
+	}else if (chosenWord === 'NewEnglandClamChowder') {
+		document.getElementById("winimg").src="./assets/images/NewEnglandClamChowder.jpg"
+	}else if (chosenWord === 'PeanutButterSandwich') {
+		document.getElementById("winimg").src="./assets/images/PeanutButterSandwich.jpg"
+	}else if (chosenWord === 'PhillyCheeseSteak') {
+		document.getElementById("winimg").src="./assets/images/PhillyCheeseSteak.jpg"
+	}else if (chosenWord === 'PoBoy') {
+		document.getElementById("winimg").src="./assets/images/PoBoy.jpg"
+	}else if (chosenWord === 'Popcorn') {
+		document.getElementById("winimg").src="./assets/images/Popcorn.jpg"
+	}else if (chosenWord === 'PotatoChips') {
+		document.getElementById("winimg").src="./assets/images/PotatoChips.jpg"
+	}else if (chosenWord === 'PotRoast') {
+		document.getElementById("winimg").src="./assets/images/PotRoast.jpg"
+	}else if (chosenWord === 'ReubenSandwich') {
+		document.getElementById("winimg").src="./assets/images/ReubenSandwich.jpg"
+	}else if (chosenWord === 'Smores') {
+		document.getElementById("winimg").src="./assets/images/Smores.jpg"
+	}else if (chosenWord === 'SourdoughBread') {
+		document.getElementById("winimg").src="./assets/images/SourdoughBread.jpg"
+	}else if (chosenWord === 'Tatertots') {
+		document.getElementById("winimg").src="./assets/images/TaterTots.jpg"
+	}else if (chosenWord === 'Twinkies') {
+		document.getElementById("winimg").src="./assets/images/Twinkies.jpg"
+	}
 }
-}
+
+function loseGraphic() {
+	//Inset Words & styles for particular word loss
+	var picturename = "<strong> You were almost able to eat " + chosenWord + "! </strong>"
+	document.getElementById('pictitle').innerHTML = picturename;
+	document.getElementById('pictitle').style.color = "white";
+	document.getElementById('pictitle').style.background = "red";
+
+	//Graphic inserted into HTML DOM if chosenWord = any of the strings below
+	if (chosenWord === 'ApplePie') {
+		document.getElementById("winimg").src="./assets/images/ApplePie.jpg"
+	}else if (chosenWord === 'BakedBeans') {
+		document.getElementById("winimg").src="./assets/images/BakedBeans.jpg"
+	}else if (chosenWord === 'BananaSplit') {
+		document.getElementById("winimg").src="./assets/images/BananaSplit.jpg"
+	}else if (chosenWord === 'BarbecueRibs') {
+		document.getElementById("winimg").src="./assets/images/BarbecueRibs.jpg"
+	}else if (chosenWord === 'BeefTacos') {
+		document.getElementById("winimg").src="./assets/images/BeefTacos.jpg"
+	}else if (chosenWord === 'BiscuitsNgravy') {
+		document.getElementById("winimg").src="./assets/images/Biscuits&gravy.jpg"
+	}else if (chosenWord === 'BLT') {
+		document.getElementById("winimg").src="./assets/images/Blt.jpg"
+	}else if (chosenWord === 'BlueBerryCobbler') {
+		document.getElementById("winimg").src="./assets/images/BlueberryCobbler.jpg"
+	}else if (chosenWord === 'BuffaloWings') {
+		document.getElementById("winimg").src="./assets/images/BuffaloWings.jpg"
+	}else if (chosenWord === 'CaliforniaRoll') {
+		document.getElementById("winimg").src="./assets/images/CaliforniaRoll.jpg"
+	}else if (chosenWord === 'CheeseBurger') {
+		document.getElementById("winimg").src="./assets/images/CheeseBurger.jpg"
+	}else if (chosenWord === 'ChicagoStylePizza') {
+		document.getElementById("winimg").src="./assets/images/ChicagoStylePizza.jpg"
+	}else if (chosenWord === 'ChocolateChipCookies') {
+		document.getElementById("winimg").src="./assets/images/ChocolateChipCookies.jpg"
+	}else if (chosenWord === 'CobbSalad') {
+		document.getElementById("winimg").src="./assets/images/CobbSalad.jpg"
+	}else if (chosenWord === 'Cornbread') {
+		document.getElementById("winimg").src="./assets/images/Cornbread.jpg"
+	}else if (chosenWord === 'Fajitas') {
+		document.getElementById("winimg").src="./assets/images/Fajitas.jpg"
+	}else if (chosenWord === 'FortuneCookies') {
+		document.getElementById("winimg").src="./assets/images/FortuneCookies.jpg"
+	}else if (chosenWord === 'FrenchFries') {
+		document.getElementById("winimg").src="./assets/images/FrenchFries.jpg"
+	}else if (chosenWord === 'FriedChickenNWaffles') {
+		document.getElementById("winimg").src="./assets/images/FriedChicken&waffles.jpg"
+	}else if (chosenWord === 'GreenChileStew') {
+		document.getElementById("winimg").src="./assets/images/GreenChileStew.jpg"
+	}else if (chosenWord === 'Grits') {
+		document.getElementById("winimg").src="./assets/images/Grits.jpg"
+	}else if (chosenWord === 'HoneyHam') {
+		document.getElementById("winimg").src="./assets/images/HoneyHam.jpg"
+	}else if (chosenWord === 'HotDogs') {
+		document.getElementById("winimg").src="./assets/images/HotDogs.jpg"
+	}else if (chosenWord === 'Jambalaya') {
+		document.getElementById("winimg").src="./assets/images/Jambalaya.jpg"
+	}else if (chosenWord === 'Jerky') {
+		document.getElementById("winimg").src="./assets/images/Jerky.jpg"
+	}else if (chosenWord === 'KeyLimePie') {
+		document.getElementById("winimg").src="./assets/images/KeyLimePie.jpg"
+	}else if (chosenWord === 'LobsterRolls') {
+		document.getElementById("winimg").src="./assets/images/LobsterRolls.jpg"
+	}else if (chosenWord === 'MacaroniNCheese') {
+		document.getElementById("winimg").src="./assets/images/Macaroni&cheese.jpg"
+	}else if (chosenWord === 'MarylandCrabcakes') {
+		document.getElementById("winimg").src="./assets/images/MarylandCrabcakes.jpg"
+	}else if (chosenWord === 'Meatloaf') {
+		document.getElementById("winimg").src="./assets/images/Meatloaf.jpg"
+	}else if (chosenWord === 'Nachos') {
+		document.getElementById("winimg").src="./assets/images/Nachos.jpg"
+	}else if (chosenWord === 'NewEnglandClamChowder') {
+		document.getElementById("winimg").src="./assets/images/NewEnglandClamChowder.jpg"
+	}else if (chosenWord === 'PeanutButterSandwich') {
+		document.getElementById("winimg").src="./assets/images/PeanutButterSandwich.jpg"
+	}else if (chosenWord === 'PhillyCheeseSteak') {
+		document.getElementById("winimg").src="./assets/images/PhillyCheeseSteak.jpg"
+	}else if (chosenWord === 'PoBoy') {
+		document.getElementById("winimg").src="./assets/images/PoBoy.jpg"
+	}else if (chosenWord === 'Popcorn') {
+		document.getElementById("winimg").src="./assets/images/Popcorn.jpg"
+	}else if (chosenWord === 'PotatoChips') {
+		document.getElementById("winimg").src="./assets/images/PotatoChips.jpg"
+	}else if (chosenWord === 'PotRoast') {
+		document.getElementById("winimg").src="./assets/images/PotRoast.jpg"
+	}else if (chosenWord === 'ReubenSandwich') {
+		document.getElementById("winimg").src="./assets/images/ReubenSandwich.jpg"
+	}else if (chosenWord === 'Smores') {
+		document.getElementById("winimg").src="./assets/images/Smores.jpg"
+	}else if (chosenWord === 'SourdoughBread') {
+		document.getElementById("winimg").src="./assets/images/SourdoughBread.jpg"
+	}else if (chosenWord === 'Tatertots') {
+		document.getElementById("winimg").src="./assets/images/TaterTots.jpg"
+	}else if (chosenWord === 'Twinkies') {
+		document.getElementById("winimg").src="./assets/images/Twinkies.jpg"
+	}
+	}
 
 function winCase (){
 	//Game finishes with '>0' guesses remaining and restarts with new word
@@ -143,43 +313,50 @@ function winCase (){
 		wins++;
 		//Push to HTML DOM
 		document.getElementById('winsTracker').innerHTML = wins;
+		alert("Press any key to continue!!");
 		//Choose random words within wordBank array & Lowercase the letters
 		chosenWord = wordBank[Math.floor(Math.random() * wordBank.length)];
 		lowerChosenWord = chosenWord.toLowerCase();
 		split = lowerChosenWord.split('');
-		//Takes raw character count
+		//Takes raw character count for chosen word
 		letterCount = split.length;
 		console.log(lowerChosenWord);
-		alert("Press any key to restart!!");
-		//Restart Game
 		document.onkeyup = function(event) {
 
-				//userGuess will store keycode used
+				//key press will begin new game
 				var userGuess = event.key;
+				alert("Press any key to restart!!");
 				startGame();
+
 }
 }
 }
 
 function loseCase () {
-	//Game finishes with '0' guesses remaining and restarts
+	//Game finishes with '0' guesses remaining
 	if (guessWrongMaximum === 0) {
+		loseGraphic();
 		losses++;
 		//Push to HTML DOM
 		document.getElementById('lossesTracker').innerHTML = losses;
+		alert("Good try, press any key to continue!!");
 		//Choose random words within wordBank array & Lowercase the letters
 		chosenWord = wordBank[Math.floor(Math.random() * wordBank.length)];
 		lowerChosenWord = chosenWord.toLowerCase();
 		split = lowerChosenWord.split('');
-		//Takes raw character count
+		//Takes raw character count for chosen word
 		letterCount = split.length;
 		console.log(lowerChosenWord);
 		//Restart Game
+		document.onkeyup = function(event) {
+
+				//key press will begin new game
+				var userGuess = event.key;
+				alert("Press any key to restart!!");
+				startGame();
 }
 }
-
-
-//Add function to if/else statement for userGuess from below (lines 182-207  )
+}
 
 function onKeyEvent() {
 	document.onkeyup = function(event) {
@@ -199,8 +376,9 @@ function onKeyEvent() {
 											for (var i = 0; i < letterCount; i++) {
 														//
 														if (lowerChosenWord[i] === userGuess) {
+																//Right Counter is increased by 1, tracking correct keystrokes used
 																rightCounter++;
-																//Updates the underScore array location with the userGuess
+																//Updates the underScore array with the userGuess-keystroke
 																underScore[i] = userGuess;
 																//Updates HTML file with the new key(s) that were used
 																document.getElementById('underscore').innerHTML = underScore.join(' ');
@@ -214,6 +392,7 @@ function onKeyEvent() {
 											wrongGuess.push(userGuess);
 											//Push to HTML DOM
 											document.getElementById('wrongGuess').innerHTML = wrongGuess;
+											//Reduces guessWrongMaximum by 1
 											guessWrongMaximum--;
 											//Push to HTML DOM
 											document.getElementById('guessWrongMaximum').innerHTML = guessWrongMaximum;
@@ -233,6 +412,7 @@ function startGame() {
 			defaultHtmlDom();
 			//Any letter will initiate below script
 			onKeyEvent();
+
 }
 
 //Calls the beginning of the game-----------------------------------------------
